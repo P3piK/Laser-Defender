4b11032ea7abe37cc810904737b05b5ae2ad1116
Pete-PC <pete@great.com>
2018-03-01 09:52:41 +0100

    Add Player's laser; working on enemies' lasers

---
+++ Assets/Scripts/EnemyBehaviour.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyBehaviour : MonoBehaviour {
+
+	public int health;
+	public GameObject laser;
+	public float speed, projectileSpeed, firingRate;
+
+
+
+	void OnTriggerEnter2D(Collider2D col)
+	{
+		Projectile missle = col.gameObject.GetComponent<Projectile> ();
+		if(missle)
+		{
+			missle.Hit();
+			health -= missle.GetDamage();
+			if(health <= 0)
+				Destroy(gameObject);
+		}
+	}
+}
---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Projectile : MonoBehaviour {
+	public int damage;
+
+	public int GetDamage()
+	{
+		return damage;
+	}
+
+	public void Hit()
+	{
+		Destroy (gameObject);
+	}
+}
